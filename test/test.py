7458460cfc5e39f0f1431cdc908e907d12c452be
cwirzenm <83652971+cwirzenm@users.noreply.github.com>
2024-09-26 17:09:10 +0100

    WIP 26th Sep

--- test/test.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import os
-
-np.load(os.path.join('C:/Users/mxnaz/OneDrive/Documents/Bath Uni/13 Dissertation/', 'labels.npy'), allow_pickle=True, encoding='latin1').item()
-
-print()
